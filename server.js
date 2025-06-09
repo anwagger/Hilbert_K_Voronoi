edb402c94ffa09867a0733770b802b1b92c5b984
anwagger <gilly@Wagtop.localdomain>
2025-06-09 13:41:55 -0400

    got js running?

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ import express from 'express'
 const app = express();
 const port = 3000;
 
+app.use(express.static('./'));
+
 app.get('/', (req, res) => {
-  res.sendFile('./index.html');
+  res.sendFile('index.html',{root:"./"});
 })
 
 app.listen(port, () => {
